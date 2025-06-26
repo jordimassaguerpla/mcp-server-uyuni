213b9fa27eb6521b2a3a3f7d6b6c4b5bdb7f2430
Jordi Massaguer Pla <jmassaguerpla@suse.com>
2025-06-26 16:58:17 +0200

    add latest patches from v0.3.0
    Signed-off-by: Jordi Massaguer Pla <jmassaguerpla@suse.com>

--- get_py_patches.sh
+++ get_py_patches.sh
@@ -1,2 +1,14 @@
-mkdir -p release/py_patches
-git format-patch --root -o release/py_patches -- '*.py'
+if [ ! -d release/py_patches ];then
+	mkdir -p release/py_patches
+fi
+if [ -f release/py_patches/last_fix ];then
+	. release/py_patches/last_fix
+	echo "last commit: $LAST_COMMIT"
+	echo "next number: $NEXT_NUMBER"
+	git format-patch $LAST_COMMIT -o release/py_patches/ --start-number $NEXT_NUMBER  -- '*.py'
+else
+	git format-patch --root -o release/py_patches -- '*.py'
+fi
+echo "Review patches in release/patches"
+echo "Update release/patches/last_fix"
+
